a0307f0a4d2200153af271a4a66d322b4ea05a65
rodede <rodede@gmail.com>
2026-01-26 12:00:02 +0200

    Generated engine structure

---
+++ bidbridge-engine/src/main/java/ro/dede/bidbridge/engine/BidBridgeEngineApplication.java
@@ -0,0 +1,13 @@
+package ro.dede.bidbridge.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidBridgeEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidBridgeEngineApplication.class, args);
+	}
+
+}
---
+++ bidbridge-engine/src/test/java/ro/dede/bidbridge/engine/BidBridgeEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.dede.bidbridge.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidBridgeEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
